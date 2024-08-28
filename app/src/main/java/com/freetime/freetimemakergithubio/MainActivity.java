27196b2995cd3bba5be43367aa44e044f43347e4
FreetimeMaker <jamieachatz@gmail.com>
2024-08-28 17:37:47 +0200

    Initial commit

---
+++ app/src/main/java/com/freetime/freetimemakergithubio/MainActivity.java
@@ -0,0 +1,23 @@
+package com.freetime.freetimemakergithubio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWeb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWeb = findViewById(R.id.myWeb);
+        myWeb.getSettings().setJavaScriptEnabled(true);
+        myWeb.setWebViewClient(new WebViewClient());
+        myWeb.loadUrl("http://freetimemaker.github.io");
+    }
+}
